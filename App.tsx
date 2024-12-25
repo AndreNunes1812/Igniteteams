3ef72d9b6312d3792787b772c2e6c613b65579f4
Andre Nunes <andre.nunes020770@gmail.com>
2024-12-25 20:48:31 -0300

    crriacao dos temas / Loading

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import React, {useState} from 'react';
 import {ThemeProvider} from 'styled-components/native';
-import theme from './src/theme';
 
+import {Loading} from '@components/Loading';
+import theme from './src/theme';
 import {Groups} from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useState(true);
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import {Container, LoadIndicator} from './styled';
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndicator color="red" />
+    </Container>
+  );
+}
