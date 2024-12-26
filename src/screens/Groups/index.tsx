e35319020e0e6deee5be7f7e2cfaf2b772d00425
Andre Nunes <andre.nunes020770@gmail.com>
2024-12-25 23:00:28 -0300

    CSS-in-JS - criando componente HEADER

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import {Header} from '@components/Header';
 import {Container, Title} from './styles';
 
 export function Groups(): React.JSX.Element {
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
     </Container>
   );
 }
